209d38929c760ccf4cce02b2e1325fada1daf7dc
Rokas214 <rokas.guzys@gmail.com>
2022-08-27 13:30:14 +0300

    Create product schema

--- src/routes/v1/createProduct.js
+++ src/routes/v1/createProduct.js
@@ -2,9 +2,28 @@ const { dbConfig } = require('../../config');
 const express = require('express');
 const mysql = require('mysql2/promise');
 const router = express.Router();
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+  img: Joi.string().min(6).max(255).required(),
+  title: Joi.string().min(6).max(255).required(),
+  author: Joi.string().min(6).max(255).required(),
+  year: Joi.number().integer().max(2022),
+  description: Joi.string().min(6).max(149).required(),
+  price: Joi.number().integer().min(0).max(2022),
+});
 
 router.post('/', async (req, res) => {
   let input = req.body;
+
+  try {
+    input = await userSchema.validateAsync(input);
+  } catch (err) {
+    return res.send({
+      err: 'Something went wrong, make sure you fill all blanks correctly',
+    });
+  }
+
   try {
     const con = await mysql.createConnection(dbConfig);
     const [data] = await con.execute(`
